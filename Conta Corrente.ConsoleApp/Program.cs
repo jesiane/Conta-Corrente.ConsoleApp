f1af79eab4df3835e3cba185764dbd47201ebc4a
Jesiane <jesi1951@hotmail.com>
2023-04-10 14:43:09 -0300

    Update Program.cs

--- Conta Corrente.ConsoleApp/Program.cs
+++ Conta Corrente.ConsoleApp/Program.cs
@@ -7,12 +7,12 @@
 
             ContaCorrente conta1 = new ContaCorrente(12, 1000, true, 0);
             ContaCorrente conta2 = new ContaCorrente(13, 300, true, 0);
-            conta1.RealizarTransferencia(conta2, 50);
             Console.WriteLine(conta1.RealizarSaque(200));
             Console.WriteLine(conta1.RealizarDeposito(300));
             Console.WriteLine(conta1.RealizarDeposito(500));
             Console.WriteLine(conta1.RealizarSaque(200));
             Console.WriteLine(conta1.RealizarTransferencia(conta2, 400));
+            Console.WriteLine(" ______________________ ");
             Console.WriteLine(conta1.ExibirExtrato());
             Console.WriteLine(conta2.ExibirExtrato());
             Console.ReadLine();
